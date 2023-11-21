15fb4905b410c958338d905e043bb770aff5370f
Alex Todeush <83349347+alex-todeush@users.noreply.github.com>
2023-11-21 20:46:27 +0000

    Formatting

--- endpoint.py
+++ endpoint.py
@@ -5,16 +5,15 @@ host_name = socket.gethostname()
 my_ip_address = socket.gethostbyname(host_name)
 
 def udp_client(port):
-    print(host_name)
-    print(my_ip_address)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-    server_address = ('<broadcast>', port)
-    header = "endpoint"
-    client_socket.sendto(header.encode('utf-8'), server_address)
+    broadcast_address = ('<broadcast>', port)
+    header = f"{my_ip_address},endpoint"
+    print(header)
+    client_socket.sendto(header.encode(), broadcast_address)
     response, _ = client_socket.recvfrom(1024)
-    print('Server response: "{}"'.format(response.decode('utf-8')))
+    print(f'Server response: "{response.decode()}"')
 
 
 if __name__ == "__main__":
@@ -23,5 +22,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     port = int(sys.argv[1])
-
     udp_client(port)
--- router.py
+++ router.py
@@ -1,6 +1,9 @@
 import socket
 import sys
 
+host_name = socket.gethostname()
+my_ip_address = socket.gethostbyname(host_name)
+
 def router(port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -12,8 +15,7 @@ def router(port):
 
     while True:
         data, client_address = server_socket.recvfrom(1024)
-        print('Received message: "{}" from {}'.format(data.decode(), client_address))
-
+        print(f'Received message: "{data.decode()}" from {client_address}')
         response = 'Server received your message'
         server_socket.sendto(response.encode(), (client_address))
 
