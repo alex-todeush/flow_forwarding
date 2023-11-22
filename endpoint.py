170cb96a2de5e9acc0c67f7de1ccbc9f0dee8ed6
Alex Todeush <83349347+alex-todeush@users.noreply.github.com>
2023-11-22 14:13:52 +0000

    Basic broadcast communication between routers

--- endpoint.py
+++ endpoint.py
@@ -4,22 +4,22 @@ import sys
 host_name = socket.gethostname()
 my_ip_address = socket.gethostbyname(host_name)
 
-def udp_client(port):
+def udp_client(port, goal_destination):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
     broadcast_address = ('<broadcast>', port)
-    header = f"{my_ip_address},endpoint"
-    print(header)
+    # HEADER FORMAT (source_IP, source_type, operation, goal_destination, placeholder)
+    header = f"{my_ip_address},endpoint,request,{goal_destination},placeholder"
     client_socket.sendto(header.encode(), broadcast_address)
     response, _ = client_socket.recvfrom(1024)
     print(f'Server response: "{response.decode()}"')
 
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print('Usage: python client.py <port>')
+    if len(sys.argv) != 3:
+        print('Usage: python3 client.py <port> <goal_destination>')
         sys.exit(1)
 
     port = int(sys.argv[1])
-    udp_client(port)
+    goal_destination = sys.argv[2]
+    udp_client(port, goal_destination)
--- router.py
+++ router.py
@@ -1,27 +1,71 @@
 import socket
 import sys
+import ipaddress
+import time
+
+def get_ip_addresses():
+    return set([i[4][0] for i in socket.getaddrinfo(socket.gethostname(), None)])
 
 host_name = socket.gethostname()
 my_ip_address = socket.gethostbyname(host_name)
+
+def get_broadcast_address(ip_address):
+    subnet_mask = "255.255.255.0"
+    network = ipaddress.IPv4Network(f"{ip_address}/{subnet_mask}", strict=False)
+    broadcast_address = network.network_address + (2 ** (32 - network.prefixlen)) - 1
+    return broadcast_address
+
+def print_forwarding_table(table):
+    print("Forwarding Table:")
+    print("{:<15} {:<15}".format("Destination", "Next Hop"))
+    for destination, next_hop in table.items():
+        print("{:<15} {:<15}".format(destination, next_hop))
 
 def router(port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
+    my_ips = get_ip_addresses()
+    my_broadcasts = [get_broadcast_address(ip) for ip in my_ips]
+
     server_address = ('', port)
     server_socket.bind(server_address)
 
-    print('UDP server is listening on port {}'.format(port))
+    print(f'UDP server is listening on {my_ip_address}')
+
+    forwarding_table = {}
+    last_seen_timestamps = {} 
 
     while True:
         data, client_address = server_socket.recvfrom(1024)
         print(f'Received message: "{data.decode()}" from {client_address}')
-        response = 'Server received your message'
-        server_socket.sendto(response.encode(), (client_address))
+
+        current_time = time.time()
+        source_ip, source_type, operation, goal_destination, placeholder = data.decode().split(',')
+
+        if source_ip in last_seen_timestamps and current_time - last_seen_timestamps[source_ip] <= 30:
+            print(f"Ignoring message from {source_ip}. Already seen in the last 30 seconds.")
+        else:
+            # Update forwarding table with source information
+            forwarding_table[source_ip] = client_address[0]
+            last_seen_timestamps[source_ip] = current_time
+            print_forwarding_table(forwarding_table)
+
+            # Check if the goal destination is in the forwarding table
+            if goal_destination in forwarding_table:
+                next_hop = forwarding_table[goal_destination]
+                header = f"{my_ip_address},router,response,{goal_destination},{next_hop}"
+                server_socket.sendto(header.encode(), (client_address[0], port))
+            else:
+                if source_ip != my_ip_address:
+                    for ip in my_broadcasts:
+                        header = f"{my_ip_address},router,inquire,{goal_destination},placeholder"
+                        broadcast_address = (str(ip), port)
+                        server_socket.sendto(header.encode(), broadcast_address)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print('Usage: python server.py <port>')
+        print('Usage: python3 server.py <port>')
         sys.exit(1)
 
     port = int(sys.argv[1])
